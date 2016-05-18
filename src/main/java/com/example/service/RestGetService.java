592cfec72e133d8f9bee88413af1b73bfc22da87
yongjunjung <codebasik@gmail.com>
2016-05-18 13:48:40 +0900

    getForObject / getForEntity 방식 및 테스트 코드추가

--- src/main/java/com/example/service/RestGetService.java
+++ src/main/java/com/example/service/RestGetService.java
@@ -2,9 +2,12 @@ package com.example.service;
 
 import com.example.model.ReceiptRequest;
 import com.example.model.ReceiptResponse;
+import com.example.util.QueryParamGenerator;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * Created by yongjunjung on 2016. 5. 17..
@@ -16,10 +19,11 @@ public class RestGetService {
     public static String ACCESS_TOKEN = "e7f1970b-68f2-4168-b128-b1239589af08";
 
     RestTemplate restTemplate = new RestTemplate();
+    QueryParamGenerator generator = new QueryParamGenerator();
 
-    public ReceiptResponse getForObjectReceipt(ReceiptRequest request) {
+    public ReceiptResponse getExchange(ReceiptRequest request) {
 
-        String RECEIPT_REQUEST_URL = getURLString(request);
+        String RECEIPT_REQUEST_URL = "http://dev-billing-api.baemin.com/receipts/" + request.getServiceId() + "/" + request.getServiceTradeNo();
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -33,8 +37,34 @@ public class RestGetService {
         return response;
     }
 
-    private String getURLString(ReceiptRequest request) {
-        return "http://dev-billing-api.baemin.com/receipts/" + request.getServiceId() + "/" + request.getServiceTradeNo();
+    public ReceiptResponse getForObject(ReceiptRequest request) {
+
+        UriComponents uriComponents = getUriComponents(request);
+        System.out.println("uriComponents = " + uriComponents.toString());
+
+        ReceiptResponse response = restTemplate.getForObject(uriComponents.toString(), ReceiptResponse.class);
+
+        return response;
     }
 
+    public ReceiptResponse getForEntity(ReceiptRequest request) {
+
+        UriComponents uriComponents = getUriComponents(request);
+        System.out.println("uriComponents = " + uriComponents.toString());
+
+        ResponseEntity<ReceiptResponse> entity = restTemplate.getForEntity(uriComponents.toString(), ReceiptResponse.class);
+        ReceiptResponse response = entity.getBody();
+
+        System.out.println("entity.getHeaders() = " + entity.getHeaders());
+
+        return response;
+    }
+
+    private UriComponents getUriComponents(ReceiptRequest request) {
+        String RECEIPT_REQUEST_URL = "http://dev-billing-api.baemin.com/test/receipts";
+
+        return UriComponentsBuilder.fromHttpUrl(RECEIPT_REQUEST_URL)
+                .queryParams(generator.generate(request))
+                .build();
+    }
 }
--- src/test/java/com/example/service/RestGetServiceTest.java
+++ src/test/java/com/example/service/RestGetServiceTest.java
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by yongjunjung on 2016. 5. 17..
  */
@@ -26,13 +24,52 @@ public class RestGetServiceTest {
     public static String SERVICE_TRADE_NO = "BEGKR02240";
 
     @Test
-    public void getForObjectReceipt() throws Exception {
+    public void getExchange() throws Exception {
 
+        //1.Given
         ReceiptRequest receiptRequest = new ReceiptRequest(SERVICE_ID, SERVICE_TRADE_NO);
-        ReceiptResponse response = restGetService.getForObjectReceipt(receiptRequest);
 
+        //2.When
+        ReceiptResponse response = restGetService.getExchange(receiptRequest);
         System.out.println("response = " + response.toString());
 
+        //3.Then
+        Assert.assertEquals("16961754", response.getApprove_no());
+        Assert.assertEquals("하나카드", response.getCard_nm());
+        Assert.assertEquals("3000", response.getAmount());
+        Assert.assertEquals("00", response.getQuota());
+        Assert.assertEquals("NM8L51W6ZL", response.getTrade_no());
+    }
+
+    @Test
+    public void getForObject() throws Exception {
+
+        //1.Given
+        ReceiptRequest receiptRequest = new ReceiptRequest(SERVICE_ID, SERVICE_TRADE_NO);
+
+        //2.When
+        ReceiptResponse response = restGetService.getForObject(receiptRequest);
+        System.out.println("response = " + response.toString());
+
+        //3.Then
+        Assert.assertEquals("16961754", response.getApprove_no());
+        Assert.assertEquals("하나카드", response.getCard_nm());
+        Assert.assertEquals("3000", response.getAmount());
+        Assert.assertEquals("00", response.getQuota());
+        Assert.assertEquals("NM8L51W6ZL", response.getTrade_no());
+    }
+
+    @Test
+    public void getForEntity() throws Exception {
+
+        //1.Given
+        ReceiptRequest receiptRequest = new ReceiptRequest(SERVICE_ID, SERVICE_TRADE_NO);
+
+        //2.When
+        ReceiptResponse response = restGetService.getForEntity(receiptRequest);
+        System.out.println("response = " + response.toString());
+
+        //3.Then
         Assert.assertEquals("16961754", response.getApprove_no());
         Assert.assertEquals("하나카드", response.getCard_nm());
         Assert.assertEquals("3000", response.getAmount());
